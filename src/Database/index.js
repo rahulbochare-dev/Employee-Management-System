50f76fd50ceaedec0d6ae3527c8bdb1d894c1829
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-07 18:51:15 +0530

    chore: Created constants.js file and created connectDB function and exported

---
+++ src/Database/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../../constants";
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Database connected successfully!", DB_NAME)
+    } catch (error) {
+        console.log("DB connection failed!")
+        process.exit(1)
+    }
+}
+
+export { connectDB }
