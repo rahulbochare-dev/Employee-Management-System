2b3e2b140bf4a4405c6b30dcb3eeeee2a84d016f
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-07 10:23:02 +0530

    Added utility files ApiError, ApiResponse and asyncHandler

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,13 @@
+class apiResponse{
+    constructor(
+        code,
+        data,
+        messege
+    ){
+        this.code = code,
+        this.data = data,
+        this.messege = messege
+    }
+}
+
+export { apiResponse }
