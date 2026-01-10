3f4ca8ea1ab1d1f67d1c2e793fe63ed77395f3d7
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-10 20:26:14 +0530

    fix: Removed extra "/" in connect function

--- src/Database/index.js
+++ src/Database/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../../constants.js";
 
 const connectDB = async() => {
     try {
-        const dbConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const dbConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
         console.log(`Database connected successfully! DB Host: ${dbConnectionInstance.connection.host}`);
     } catch (error) {
         console.log("DB connection failed!", error);
