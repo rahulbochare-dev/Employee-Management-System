b82f2acb08a200748fecaad0527fe6bf5bd4555f
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-12 11:53:40 +0530

    fix: Imported and configured dotenv in cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,10 @@
+import dotenv from "dotenv"
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+
+dotenv.config({
+    path: "./.env"
+})
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
