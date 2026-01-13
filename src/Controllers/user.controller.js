12a8917f702c45db92ba91099084c21bcd2cd29f
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-13 19:31:04 +0530

    feat: Created function to genarate access and refresh token

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -6,6 +6,23 @@ import { User } from "../Models/user.model.js"
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken"
 
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+    
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+    
+        return {accessToken, refreshToken}
+        
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens!")
+    }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
     const {firstName, lastName, userName, email, avatar, dateOfBirth, country, city, timezone, role, password, adminSecret} = req.body
 
