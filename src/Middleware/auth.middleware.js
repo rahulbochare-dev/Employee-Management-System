454b9e5836576ee10922983d6b46bd5db2e34667
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-13 20:54:36 +0530

    feat: Created auth.middleware.js file and cerated verifyJWT function and exported

---
+++ src/Middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+import  jwt  from "jsonwebtoken"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../Models/user.model.js"
+
+const verifyToken = async (req, res, next) => {
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ",  "")
+        
+        if(!accessToken){
+            throw new ApiError(401, "Unauthorized request!")
+        }
+        
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+        
+        const user = await User.findById(decodedToken?._id).select("-password -adminSecret -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401, "Invalid access token!")
+        }
+
+        console.log(user)
+    
+        req.user = user
+        next()
+
+    } catch (error) {
+        throw new ApiError(401, "Something went wrong while parsing token!")
+    }
+}
+
+export { verifyToken }
