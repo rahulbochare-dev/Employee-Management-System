6510f4ca2a9a6ef1c33b6d670294dbd04b4f1fef
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-14 11:48:00 +0530

    fix: Fixed the wrong controller in route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -18,6 +18,6 @@ router.route("/register").post(upload.fields(
 router.route("/login").post(loginUser)
 
 // secured routes
-router.route("/logout").post(verifyToken, loginUser)
+router.route("/logout").post(verifyToken, logoutUser)
 
 export default router
