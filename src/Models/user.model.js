5e2b765305d1ba853cbd34e03f3dfb775dfccd84
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-14 19:03:06 +0530

    fix: Removed unused import statement in user.model.js

--- src/Models/user.model.js
+++ src/Models/user.model.js
@@ -1,7 +1,6 @@
 import { mongoose, Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
-import { type } from "os";
 
 const userSchema = new Schema({
     firstName: {
