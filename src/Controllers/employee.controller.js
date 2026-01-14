b64b54798fb0e13a21658c85c73e805cda0259eb
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-14 19:19:07 +0530

    feat: Created employee.controller.js file and added onboardEmployee controller and exported

---
+++ src/Controllers/employee.controller.js
@@ -0,0 +1,9 @@
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Employee } from "../Models/employee.model.js"
+
+const onboardEmployee = asyncHandler( async (req, res) => {
+    
+})
