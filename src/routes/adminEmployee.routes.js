e1c379c24e2a02563e5d68240ba551ae7cbb1926
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-14 19:45:02 +0530

    feat: Created onboard employee route

--- src/routes/adminEmployee.routes.js
+++ src/routes/adminEmployee.routes.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
 import { upload } from "../Middleware/multer.middleware.js"
+import { onboardEmployee } from "../Controllers/adminEmployee.controller.js"
 
 const router = Router()
+
+router.route("/onboard-employee").post(upload.fields(
+    [
+        {
+            name: "avatar",
+            maxCount: 1
+        }
+    ]
+))
 
 export default router
