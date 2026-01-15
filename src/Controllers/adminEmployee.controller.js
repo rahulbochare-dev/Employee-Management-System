1d418d5f08faa6a8575a9ea8129ebd7902929c02
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-15 19:46:09 +0530

    feat: Added check if any fields are not empty

--- src/Controllers/adminEmployee.controller.js
+++ src/Controllers/adminEmployee.controller.js
@@ -7,6 +7,11 @@ import { Employee } from "../Models/employee.model.js"
 const onboardEmployee = asyncHandler( async (req, res) => {
     const {empID, firstName, lastName, email, gender, contactNo, workMode, avatar, dateOfBirth, country, city, pinCode, address, role, isActive, salary, joinedAt, password} = req.body
     
+    if(
+        [empID, firstName, lastName, email, gender, contactNo, workMode, avatar, dateOfBirth, country, city, pinCode, address, role, isActive, salary, joinedAt, password].some((fields) => (fields === ""))
+    ){
+        throw new ApiError(400, "Empty fields are not accepted!")
+    }
 })
 
 export { onboardEmployee }
