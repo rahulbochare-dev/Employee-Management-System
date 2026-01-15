ddcda14f752437fbfcd3c2ac7297f7351f10010d
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-15 20:43:41 +0530

    feat: Accepted and validated salaryCurrency field

--- src/Controllers/adminEmployee.controller.js
+++ src/Controllers/adminEmployee.controller.js
@@ -5,10 +5,10 @@ import { ApiError } from "../utils/ApiError.js";
 import { Employee } from "../Models/employee.model.js"
 
 const onboardEmployee = asyncHandler( async (req, res) => {
-    const {empID, firstName, lastName, email, gender, contactNo, workMode, avatar, dateOfBirth, country, city, pinCode, address, role, isActive, salary, joinedAt, password} = req.body
+    const {empID, firstName, lastName, email, gender, contactNo, workMode, avatar, dateOfBirth, country, city, pinCode, address, role, isActive, salary, salaryCurrency joinedAt, password} = req.body
     
     if(
-        [empID, firstName, lastName, email, gender, contactNo, workMode, avatar, dateOfBirth, country, city, pinCode, address, role, isActive, salary, joinedAt, password].some((fields) => (fields === ""))
+        [empID, firstName, lastName, email, gender, contactNo, workMode, avatar, dateOfBirth, country, city, pinCode, address, role, isActive, salary, salaryCurrency, joinedAt, password].some((fields) => (fields === ""))
     ){
         throw new ApiError(400, "Empty fields are not accepted!")
     }
