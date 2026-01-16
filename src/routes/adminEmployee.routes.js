b4e558c94a9c1a27d1b6b0038255fcb7eca07792
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-16 11:56:46 +0530

    feat: imported getAllEmployees controller and created route

--- src/routes/adminEmployee.routes.js
+++ src/routes/adminEmployee.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../Middleware/multer.middleware.js"
-import { onboardEmployee } from "../Controllers/adminEmployee.controller.js"
+import { onboardEmployee, getAllEmployees } from "../Controllers/adminEmployee.controller.js"
 import { checkIsAdmin } from "../Middleware/checkAdmin.middleware.js";
 
 const router = Router()
@@ -13,5 +13,7 @@ router.route("/onboard-employee").post(upload.fields(
         }
     ]
 ), checkIsAdmin, onboardEmployee)
+
+router.route("/all-employees").get(checkIsAdmin, getAllEmployees)
 
 export default router
