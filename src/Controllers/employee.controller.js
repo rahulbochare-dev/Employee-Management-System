de464634434ba173a8f72cc719f919d8faf2738a
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-22 12:05:47 +0530

    feat: Created employee controller file

---
+++ src/Controllers/employee.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Employee } from "../Models/employee.model.js"
+
+const loginEmployee = asyncHandler( async (req, res) => {
+
+})
+
+export { loginEmployee }
