43ac43f8b3dceb58e5f7e5489b858dbb33bf0806
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-22 12:12:15 +0530

    faet: Created checkIsEmployee midleware and exported

--- src/Middleware/checkEmployee.middleware.js
+++ src/Middleware/checkEmployee.middleware.js
@@ -2,3 +2,9 @@ import  jwt  from "jsonwebtoken"
 import { ApiError } from "../utils/ApiError.js"
 import { Employee } from "../Models/employee.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+
+const checkIsEmployee = asyncHandler( async (req, res, next) => {
+    
+})
+
+export { checkIsEmployee }
