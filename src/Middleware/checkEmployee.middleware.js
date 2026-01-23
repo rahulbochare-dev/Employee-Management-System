0232737effafaa87b13463f14b4687d9e5aec615
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-23 11:20:24 +0530

    feat: Added employee role check and user logic in checkEmployee middleware

--- src/Middleware/checkEmployee.middleware.js
+++ src/Middleware/checkEmployee.middleware.js
@@ -4,7 +4,29 @@ import { Employee } from "../Models/employee.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const checkIsEmployee = asyncHandler( async (req, res, next) => {
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        if(!accessToken){
+            throw new ApiError(401, "Unauthorized request!")
+        }
     
+        const decodedToken = jwt.sign(accessToken, process.env.ACCESS_TOKEN_SECRET)
+        const user = Employee.findOne(decodedToken._id).select("-password -refreshToken")
+        
+        if(!user){
+            throw new ApiError(404, "Invalid access token!")
+        }
+    
+        if(!user.role === "employee"){
+            throw new ApiError(401, "You dont have employee access!")
+        }
+    
+        req.user = user
+        next()
+        
+    } catch (error) {
+        throw new ApiError(401, "Something went wrong while parsing token!")
+    }
 })
 
 export { checkIsEmployee }
