fbced1c7a544ea23e565e9b0c4befc66a78e517c
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-23 11:38:33 +0530

    feat: Added function to generate access and refresh token

--- src/Controllers/employee.controller.js
+++ src/Controllers/employee.controller.js
@@ -3,6 +3,23 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Employee } from "../Models/employee.model.js"
 
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        
+        const accessToken = await user.generateAccessToken()
+        const refreshToken = await user.generateRefreshToken()
+        
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+        return {accessToken, refreshToken}
+    
+        
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens!")
+    }
+}
+
 const loginEmployee = asyncHandler( async (req, res) => {
     const {email, password} = req.body
 
