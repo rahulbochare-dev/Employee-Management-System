3e20ec8f2a0e7b0f18009b4e8b2a86ed5330d14a
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-23 20:17:10 +0530

    feat: Imported reset password controller and created route with role based access

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { checkIsEmployee } from "../Middleware/checkEmployee.middleware.js";
-import { loginEmployee } from "../Controllers/employee.controller.js"
+import { loginEmployee, resetPassword } from "../Controllers/employee.controller.js"
 
 const router = Router()
 
 router.route("/login").post(loginEmployee)
+router.route("/reset-password").post(checkIsEmployee, resetPassword)
 
 export default router
