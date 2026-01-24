a5dd18d34eadb24db08f8b82421805aea50a6489
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-24 11:37:35 +0530

    feat: Imported logout employee controller and created route

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { checkIsEmployee } from "../Middleware/checkEmployee.middleware.js";
-import { loginEmployee, resetPassword, getCurrentEmployeeDetails } from "../Controllers/employee.controller.js"
+import { loginEmployee, resetPassword, getCurrentEmployeeDetails, logoutEmployee } from "../Controllers/employee.controller.js"
 
 const router = Router()
 
 router.route("/login").post(loginEmployee)
 router.route("/reset-password").post(checkIsEmployee, resetPassword)
 router.route("/employee-details").post(checkIsEmployee, getCurrentEmployeeDetails)
+router.route("/logout").post(checkIsEmployee, logoutEmployee)
 
 export default router
