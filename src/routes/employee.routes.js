0c90b5f81a3d7ecfb913016e248182cf6c04b885
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-24 11:19:11 +0530

    feat: imported get employee details controller and created route

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { checkIsEmployee } from "../Middleware/checkEmployee.middleware.js";
-import { loginEmployee, resetPassword } from "../Controllers/employee.controller.js"
+import { loginEmployee, resetPassword, getCurrentEmployeeDetails } from "../Controllers/employee.controller.js"
 
 const router = Router()
 
 router.route("/login").post(loginEmployee)
 router.route("/reset-password").post(checkIsEmployee, resetPassword)
+router.route("/employee-details").post(checkIsEmployee, getCurrentEmployeeDetails)
 
 export default router
