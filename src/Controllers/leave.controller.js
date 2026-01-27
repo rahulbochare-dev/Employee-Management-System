7ca3c5ba23d247110844bb49cc9bb3c89b33b30e
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-27 12:31:42 +0530

    feat: Added necessery validations on fields accepted from req

--- src/Controllers/leave.controller.js
+++ src/Controllers/leave.controller.js
@@ -4,6 +4,17 @@ import { ApiError } from "../utils/ApiError.js";
 import { Leave } from "../Models/leave.model.js"
 
 const addLeave = asyncHandler( async (req, res) => {
+    const {leaveType, description, from, to} = req.body
+
+    if(
+        [leaveType, description, from, to].some((fields) => (fields.trim() === ""))
+    ){
+        throw new ApiError(400, "Empty fields are not accepted!")
+    }
+
+    if(description.length < 50){
+        throw new ApiError(400, "Description must be at least 50 charecters long!")
+    }
 
 })
 
