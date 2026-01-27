ccf35485c6f3893c5f126d498dcca8ec229798a8
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-27 20:29:16 +0530

    feat: Created the leave in DB and returned in res

--- src/Controllers/leave.controller.js
+++ src/Controllers/leave.controller.js
@@ -16,6 +16,15 @@ const addLeave = asyncHandler( async (req, res) => {
         throw new ApiError(400, "Description must be at least 50 charecters long!")
     }
 
+    const createdLeave = await Leave.create({
+        employee: req.employee._id,
+        leaveType: leaveType,
+        description: description,
+        from: from,
+        to: to
+    })
+
+    res.status(200).json(new ApiResponse(200, {leave: createdLeave}, "Leave submitted successfully"))
 })
 
 export { addLeave }
