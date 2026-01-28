d845209c7bdf3408b3fd9f029b7be59df69e0341
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-28 20:10:50 +0530

    refactor: Changed file name in Import statement

--- src/routes/leave.routes.js
+++ src/routes/leave.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { checkIsEmployee } from "../Middleware/checkEmployee.middleware.js";
-import { addLeave } from "../Controllers/leave.controller.js"
+import { addLeave } from "../Controllers/employeeLeave.controller.js"
 import router from "./employee.routes.js";
 
 router.route("/add-leave").post(checkIsEmployee, addLeave)
