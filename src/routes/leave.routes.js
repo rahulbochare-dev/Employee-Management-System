16e298e82cbc59e8702ad2124c18dbebc4eb049c
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-28 20:11:44 +0530

    feat: Imported getLeaves controller and created route

--- src/routes/leave.routes.js
+++ src/routes/leave.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { checkIsEmployee } from "../Middleware/checkEmployee.middleware.js";
-import { addLeave } from "../Controllers/employeeLeave.controller.js"
+import { addLeave, getLeaves } from "../Controllers/employeeLeave.controller.js"
 import router from "./employee.routes.js";
 
 router.route("/add-leave").post(checkIsEmployee, addLeave)
+router.route("/leaves").post(checkIsEmployee, getLeaves)
 
 export default router
