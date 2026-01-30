bd34bd10da2087e564f4502a2e1ade0df08792e3
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-01-30 19:28:31 +0530

    feat: Added controller to update leave status and exported

--- src/Controllers/adminLeave.controller.js
+++ src/Controllers/adminLeave.controller.js
@@ -4,5 +4,24 @@ import { ApiError } from "../utils/ApiError.js";
 import { Leave } from "../Models/leave.model.js"
 
 const updateLeaveStatus = asyncHandler( async (req, res) => {
-    
+    const {leaveId, status} = req.query
+
+    if([leaveId, status].some((fields) => (fields.trim() === ""))){
+        throw new ApiError(400, "Empty fields are not accepted!")
+    }
+
+    const leaveInDB = await Leave.findById(leaveId)
+
+    if(!leaveInDB){
+        throw new ApiResponse(404, "Leave does not exists!")
+    }
+
+    const upadtedLeave = await leaveId.save({
+        status: status
+    })
+
+    return res.status(200).json(new ApiResponse(200, {leave: upadtedLeave}, "Leave updated successfully"))
+
 })
+
+export { updateLeaveStatus }
