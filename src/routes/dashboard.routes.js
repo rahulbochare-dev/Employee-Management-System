86695adcc18b857c2207c9694d46c3fa54153cec
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-09 18:13:44 +0530

    feat: Created route and changed route name

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { checkIsAdmin } from "../middlewares/checkAdmin.middleware.js";
-import { getEmployeeGenderRatio } from "../controllers/dashboard.controller.js";
+import { getEmployeeGenderRatio, getPendingLeaveApplications } from "../controllers/dashboard.controller.js";
 
 const router = Router()
 
-router.route("/employeeGenderRatio").get(checkIsAdmin, getEmployeeGenderRatio)
+router.route("/employee-gender-ratio").get(checkIsAdmin, getEmployeeGenderRatio)
+router.route("/pending-leave-applications").get(checkIsAdmin, getPendingLeaveApplications)
 
 export default router
