b85dee2e689e154d5df905b17c1626b8a10094e4
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-09 12:40:59 +0530

    feat: Created aggregation pipeline to get gender ratio

--- src/Controllers/dashboard.controller.js
+++ src/Controllers/dashboard.controller.js
@@ -2,3 +2,22 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Employee } from "../models/employee.model.js"
+
+const getEmployeeGenderRatio = asyncHandler( async (req, res) => {
+    const employeeRatio = await Employee.aggregate([{
+        $group: {
+            _id: "$gender",
+            total: {
+                $sum: 1
+            }
+        }
+    }])
+
+    if(!employeeRatio){
+        throw new ApiError(400, "Employees now found!")
+    }
+
+    res.status(200).json(new ApiResponse(200, employeeRatio, "Employee ratio fetched succesfully"))
+})
+
+export { getEmployeeGenderRatio }
