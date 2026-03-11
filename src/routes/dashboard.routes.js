2f457a6738dfc550f2ab55dd372fc19c3a36513e
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-11 12:21:20 +0530

    feat: Imported controller and created new route

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { checkIsAdmin } from "../middlewares/checkAdmin.middleware.js";
-import { getEmployeeGenderRatio, getPendingLeaveApplications } from "../controllers/dashboard.controller.js";
+import { getEmployeeGenderRatio, getOnLeaveToday, getPendingLeaveApplications } from "../controllers/dashboard.controller.js";
 
 const router = Router()
 
 router.route("/employee-gender-ratio").get(checkIsAdmin, getEmployeeGenderRatio)
 router.route("/pending-leave-applications").get(checkIsAdmin, getPendingLeaveApplications)
+router.route("/on-leave-today").get(checkIsAdmin, getOnLeaveToday)
 
 export default router
