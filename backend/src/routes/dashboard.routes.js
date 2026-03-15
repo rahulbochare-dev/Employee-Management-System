0c4c4773d03377efb417992e1f44bd321a7e8f73
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-15 18:54:03 +0530

    feat: Imported last week leaves controller and created route

--- backend/src/routes/dashboard.routes.js
+++ backend/src/routes/dashboard.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { checkIsAdmin } from "../middlewares/checkAdmin.middleware.js";
-import { getEmployeeGenderRatio, getNewJoinesThisMonth, getOnLeaveToday, getPendingLeaveApplications } from "../controllers/dashboard.controller.js";
+import { getEmployeeGenderRatio, getLastWeeksLeaves, getNewJoinesThisMonth, getOnLeaveToday, getPendingLeaveApplications } from "../controllers/dashboard.controller.js";
 
 const router = Router()
 
@@ -8,5 +8,6 @@ router.route("/employee-gender-ratio").get(checkIsAdmin, getEmployeeGenderRatio)
 router.route("/pending-leave-applications").get(checkIsAdmin, getPendingLeaveApplications)
 router.route("/on-leave-today").get(checkIsAdmin, getOnLeaveToday)
 router.route("/new-joines").get(checkIsAdmin, getNewJoinesThisMonth)
+router.route("/last-week-leaves").get(checkIsAdmin, getLastWeeksLeaves)
 
 export default router
