cad04cc06ba21b26fcdb1f2a2f9c686da1ac0cb2
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-22 20:47:29 +0530

    feat: Created kipCard component and page dashboard

---
+++ frontend/src/components/KPICard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const KPICard = () => {
+  return (
+    <>
+    <div className='w-87 h-41.5 border border-[#8f8f8f] rounded-2xl'>
+        <div className='w-full h-[6.688rem] rounded-t-2xl border-b border-[#8f8f8f]'>
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default KPICard
