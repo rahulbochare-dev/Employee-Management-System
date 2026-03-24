063201d0b8a164c37fa81131af4cf7ed25ee8364
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-24 19:57:13 +0530

    feat: Made changes in card component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <>
-     <KPICard/> 
+     <KPICard
+     title={"Total Employees"}/> 
     </>
   )
 }
--- frontend/src/components/KPICard.jsx
+++ frontend/src/components/KPICard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const KPICard = ({title, mainIcon, icon2, icon3}) => {
   return (
     <>
-    <div className='w-87 h-41.5 border border-[#8f8f8f] rounded-2xl overflow-clip'>
-        <div className='w-full h-[6.688rem] rounded-t-2xl border-b border-[#8f8f8f] flex'>
+    <div className='w-87 h-41.5 border border-[#b6b6b6] rounded-2xl overflow-clip'>
+        <div className='w-full h-[6.688rem] rounded-t-2xl border-b border-[#b6b6b6] flex'>
           <div className='w-1/2 h-[6.688rem]'>
               <h3 className='text-lg font-medium text-[#7D7D7D] pl-[1.313rem] pt-[0.938rem]'>{title}</h3>
               <h1 className='text-5xl font-semibold pl-[1.313rem] pt-1'>133</h1>
@@ -14,9 +14,9 @@ const KPICard = ({title, mainIcon, icon2, icon3}) => {
           </div>
         </div>
         <div className='w-full h-[3.610rem] rounded-b-2xl overflow-clip flex'>
-          <div className='w-1/3 h-full flex pl-4 justify-start items-center borr'>
+          <div className='w-4/8 h-full flex pl-4 justify-start items-center'>
             <img className='w-8 mr-2' src="\src\assets\male.svg" alt="" srcset="" />
-            <h3 className='text-md font-semibold text-[#7D7D7D]'>70 Male</h3>
+            <h3 className='text-md font-semibold border-r border-[#b6b6b6] w-22 text-[#7D7D7D]'>70 Male</h3>
           </div>
           <div className='w-2/3 h-full flex pl-4 justify-start items-center'>
             <img className='w-8 mr-2' src="\src\assets\female.svg" alt="" srcset="" />
