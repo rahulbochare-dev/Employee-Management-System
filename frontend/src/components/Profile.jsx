48bb00e3df1bb1e89e7c8ded31ba681ea38acd90
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-25 11:48:26 +0530

    feat: Changes in profile component

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div></div>
+    <div className=' w-fit h-10 flex'>
+        <div className=' w-[18%] h-full'>
+            <img className='w-full h-full' src="src/assets/account.svg" alt="" srcset="" />
+        </div>
+        <div className='w-[82%] h-full pl-2'>
+            <h3 className='text-[0.9375rem] font-medium'>Eren Moorehead</h3>
+            <h4 className='text-[0.80rem] mt-[-1%] text-[#707070] '>erenmoorhead15@gmail.com</h4>
+        </div>
+    </div>
   )
 }
 
