5ddb139940467e0044c1ce51e2c03e388a80e809
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-27 10:48:04 +0530

    feat: Designed WelcomeText component

--- frontend/src/components/WelcomeText.jsx
+++ frontend/src/components/WelcomeText.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const WelcomeText = () => {
+const WelcomeText = ({name}) => {
   return (
-    <div>WelcomeText</div>
+    <div className='w-fit h-fit'>
+        <h2 className='text-[2.5rem] text-[#919191]'>Welcome, <span className='text-black'>{name || "User"}</span></h2>
+    </div>
   )
 }
 
