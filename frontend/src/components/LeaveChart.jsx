e62181c436d2b7bdf8f12e582e4a281c2a077694
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-03-29 12:46:59 +0530

    feat: Designed leave chart component

--- frontend/src/components/LeaveChart.jsx
+++ frontend/src/components/LeaveChart.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import { BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis, Bar } from 'recharts'
+
+const data = [
+    { day: 'Mon', count: 28 },
+    { day: 'Tue', count: 38 },
+    { day: 'Wed', count: 32 },
+    { day: 'Thu', count: 48 },
+    { day: 'Fri', count: 35 },
+    { day: 'Sat', count: 20 },
+];
 
 const LeaveChart = () => {
     return (
@@ -13,6 +23,32 @@ const LeaveChart = () => {
                     <span className='font-light'> / </span>5.57
                     <span className='text-[1.25rem] text-[#707070] font-normal'> Avg/day</span>
                 </h1>
+            </div>
+            <div className="w-full h-74 rounded-b-[0.9375rem] pt-1">
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        data={data}
+                        layout="vertical"
+                        margin={{ top: 5, right: 30, left: 10, bottom: 5 }}
+                    >
+                        <CartesianGrid horizontal={false} stroke="#707070" strokeWidth={0.2} />
+                        <XAxis type="number" axisLine={false} tickLine={false} />
+                        <YAxis
+                            dataKey="day"
+                            type="category"
+                            axisLine={false}
+                            tickLine={false}
+                            tick={{ fill: '#707070', fontSize: 16 }}
+                            width={60}
+                        />
+                        <Bar
+                            dataKey="count"
+                            fill="#9E6EFF"
+                            radius={[0, 5, 5, 0]}
+                            barSize={25}
+                        />
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         </div>
     )
