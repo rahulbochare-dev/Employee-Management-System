12d1edc700fba5e6b2dce7e49bf84ce4760d32b9
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-04-06 12:56:48 +0530

    refactor: Added hover state in button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-const Button = ({name, icon}) => {
+const Button = ({ name, icon }) => {
 
   return (
-    <div className={"w-63.5 h-10 rounded-lg flex items-center justify-center gap-3 mt-5 cursor-pointer transition-all duration-200 bg-black"}>
-        
-            <img className='w-5' src={icon} alt="" srcset="" />
-            <h3 className={"font-medium text-white"}>{name}</h3>
-        
+    <div className={"w-63.5 h-10 rounded-lg flex items-center justify-center gap-3 mt-5 cursor-pointer transition-all duration-200 bg-black hover:bg-gray-900"}>
+      <img className='w-5' src={icon} alt="" srcset="" />
+      <h3 className={"font-medium text-white"}>{name}</h3>
     </div>
   )
 }
