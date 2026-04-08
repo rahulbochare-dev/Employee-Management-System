23c01a968337364889835f3200aeb78bd420e648
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-04-08 13:32:23 +0530

    refactor: Changed prop name

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-const Button = ({ name, icon, marginY }) => {
+const Button = ({ title, icon, marginY }) => {
 
   return (
     <div className={`w-63.5 h-10 rounded-lg flex items-center justify-center gap-3 ${marginY} cursor-pointer transition-all duration-200 bg-black hover:bg-gray-900`}>
       <img className='w-5' src={icon} alt="" srcset="" />
-      <h3 className={"font-medium text-white"}>{name}</h3>
+      <h3 className={"font-medium text-white"}>{title}</h3>
     </div>
   )
 }
