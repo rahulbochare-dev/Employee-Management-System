81cb607026cab1991bd969408919b3c5e4b697f5
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-04-08 13:37:42 +0530

    refactor: Changed width in Button component

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ title, icon, marginY }) => {
 
   return (
     <div className={`w-63.5 h-10 rounded-lg flex items-center justify-center gap-3 ${marginY} cursor-pointer transition-all duration-200 bg-black hover:bg-gray-900`}>
-      <img className='w-5' src={icon} alt="" srcset="" />
+      <img className='w-6' src={icon} alt="" srcset="" />
       <h3 className={"font-medium text-white"}>{title}</h3>
     </div>
   )
