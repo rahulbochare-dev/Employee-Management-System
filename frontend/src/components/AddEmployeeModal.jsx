612554023a3d568355cee71b70347cdbb95019e5
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-04-09 18:46:05 +0530

    feat: Finished add employee modal component

--- frontend/src/components/AddEmployeeModal.jsx
+++ frontend/src/components/AddEmployeeModal.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Separator from './Seperator.jsx'
 import TextInput from './TextInput.jsx'
 import DropdownAddEmployee from './DropdownAddEmployee.jsx'
 import countries from "../data/contries.json";
 import FileSelect from './FileSelect.jsx';
+import Button from './Button.jsx';
 
 const AddEmployeeModal = () => {
+    const [showModal, setShowModal] = useState(true)
+
+    const handleShowModal = (e) => {
+        setShowModal(!showModal)
+    }
+
   return (
     <>
-        <div className="w-screen h-screen z-10 flex justify-center items-center">
+        {showModal && <div className="w-screen h-screen z-10 flex justify-center items-center">
             <div className="w-330.5 h-173 bg-white border border-[#b6b6b6] rounded-2xl overflow-clip">
                 <div className="w-full h-12 flex items-center pt-3">
                     <h1 className='text-[1.75rem] font-semibold pl-10'>Onboard Employee</h1>
@@ -30,21 +37,24 @@ const AddEmployeeModal = () => {
                     <TextInput label={"Last Name:"} placeholder={"Last Name"}/>
                     <TextInput label={"Date of Birth:"} placeholder={"Date of Birth"}/>
                     <TextInput label={"Role:"} placeholder={"Role"}/>
-                    {/* <TextInput label={"Avatar:"} placeholder={"Avatar"}/> */}
                     <FileSelect label={"Avatar:"} placeholder={"Choose Avatar"}/>
                     <DropdownAddEmployee label={"Gender:"} title={"Gender"} values={["Male", "Female"]}/>
                     <DropdownAddEmployee label={"Country:"} title={"Country"} values={countries}/>
                     <DropdownAddEmployee label={"Workmode:"} title={"Workmode"} values={["On-Site", "Remote", "Hybrid"]}/>
                     <DropdownAddEmployee label={"Employement Type:"} title={"Employement Type"} values={["Full Time", "Contract", "Freelance", "Intern"]}/>
                 </div>
-                <div className="w-full h-21 bg-amber-200 flex">
-                    <div className="w-4/6 h-full bg-blue-300"></div>
-                    <div className="w-2/6 h-full bg-blue-500">
-                    
+                <div className="w-full h-21 flex">
+                    <div className="w-4/6 h-full pt-6 flex flex-col">
+                        <Separator width={"w-199"}/>
+                        <h4 className='ml-11 mt-2 text-[#929292] italic'>Tip: Joining date is generated automatically.</h4>
+                    </div>
+                    <div className="w-2/6 h-full flex items-center gap-8 justify-end pr-10">
+                        <Button title={"Cancel"} width={"w-36"} secondary={true} onClick={handleShowModal}/>
+                        <Button title={"Add Employee"} icon={"/src/assets/employeeAdd-Light.svg"} width={"w-48"} onClick={handleShowModal}/>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>}
     </>
   )
 }
--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
 
-const Button = ({ title, icon, marginY, onClick }) => {
+const Button = ({ title, icon, marginY, width = "w-63.5", onClick, secondary = false }) => {
 
   return (
-    <button onClick={onClick} className={`w-63.5 h-10 rounded-lg flex items-center justify-center gap-3 ${marginY} cursor-pointer transition-all duration-200 bg-black hover:bg-gray-900`}>
-      <img className='w-6' src={icon} alt="" srcset="" />
-      <h3 className={"font-medium text-white"}>{title}</h3>
+    <button
+      onClick={onClick}
+      className={`${secondary ? `${width} h-10 rounded-lg flex items-center justify-center bg-white border border-[#b6b6b6] ${marginY} cursor-pointer transition-all duration-200 hover:bg-gray-200` : `${width} h-10 rounded-lg flex items-center justify-center gap-3 ${marginY} cursor-pointer transition-all duration-200 bg-black hover:bg-gray-900`}`}>
+      {secondary || <img className='w-6' src={icon} alt="" srcset="" />}
+      <h3 className={`font-medium ${secondary ? "text-black" : "text-white"} `}>{title}</h3>
     </button>
   )
 }
