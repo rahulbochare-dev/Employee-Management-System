b8fe359a0655da529ca27b36def3ebd76e509df3
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-04-09 18:19:17 +0530

    feat: Added prop in button component

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-const Button = ({ title, icon, marginY }) => {
+const Button = ({ title, icon, marginY, onClick }) => {
 
   return (
-    <div className={`w-63.5 h-10 rounded-lg flex items-center justify-center gap-3 ${marginY} cursor-pointer transition-all duration-200 bg-black hover:bg-gray-900`}>
+    <button onClick={onClick} className={`w-63.5 h-10 rounded-lg flex items-center justify-center gap-3 ${marginY} cursor-pointer transition-all duration-200 bg-black hover:bg-gray-900`}>
       <img className='w-6' src={icon} alt="" srcset="" />
       <h3 className={"font-medium text-white"}>{title}</h3>
-    </div>
+    </button>
   )
 }
 
