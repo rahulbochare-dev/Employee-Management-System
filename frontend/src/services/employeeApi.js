849b4f60378342f32cb4aee2a242fab08f437dc3
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-04-14 20:41:14 +0530

    feat: Installed zustand

--- frontend/src/services/employeeApi.js
+++ frontend/src/services/employeeApi.js
@@ -2,7 +2,6 @@ import { callApi } from "../utils/callAPI.js";
 
 const getEmployees = function(){
     const response = callApi("GET", "admin/dashboard/employee-gender-ratio")
-    console.log(response)
     return response
 }
 
