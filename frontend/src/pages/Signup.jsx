7adba44df8b8d7572c5a1d197cd26de3ef50a724
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-03 20:58:49 +0530

    feat: Setup navigate in signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 import TextInput from '../components/TextInput.jsx'
 import FileSelect from '../components/FileSelect.jsx'
 import DateSelect from '../components/DateSelect.jsx'
 import DropdownAddEmployee from '../components/DropdownAddEmployee.jsx'
 import PasswordInput from '../components/PasswordInput.jsx'
+import Button from '../components/Button.jsx'
 
 const Signup = () => {
+    const navigate = useNavigate()
+
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-[#f9f9f9]'>
         <div className="bg-white w-2/5 h-4/5 rounded-3xl">
         <div className='w-full h-20 flex justify-center items-center'>
             <h1 className="text-4xl">Signup</h1>
         </div>
-        <div className="w-full h-123 bg-red-200 grid grid-cols-3 items-center justify-items-center">
+        <div className="w-full h-123 grid grid-cols-3 items-center justify-items-center">
             <TextInput label={"First Name:"} placeholder={"First Name"}/>
             <TextInput label={"Last Name:"} placeholder={"Last Name"}/>
             <TextInput label={"Email:"} placeholder={"Email"}/>
@@ -23,6 +27,10 @@ const Signup = () => {
             <DropdownAddEmployee label={"Select Role:"} values={["admin", "employee"]}/>
             <PasswordInput placeholder={"Password"} label={"Enter Password"}/>
             <PasswordInput label={"Enter Admin Secret:"} placeholder={"Admin Secret"}/>
+        </div>
+        <div className="w-full h-47 flex justify-center items-center flex-col gap-5">
+            <Button width='w-64' title={"Signup"} icon={"/src/assets/login.svg"}/>
+            <h3>Already have an account <span onClick={navigate("/login")} className='text-blue-500 cursor-pointer underline'>Login</span></h3>
         </div>
         </div>
     </div>
