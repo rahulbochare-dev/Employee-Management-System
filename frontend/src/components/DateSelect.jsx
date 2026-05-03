d430c2c9f084e4165e2674b7b540ba2072ca7a0d
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-03 20:40:24 +0530

    feat: Created dateselect component and Created design for signup page

---
+++ frontend/src/components/DateSelect.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const DateSelect = ({ label, placeholder }) => {
+    const [inputValue, setInputValue] = useState("")
+
+    const handleDateChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+  return (
+    <div className="w-56.5 h-16 flex flex-col justify-between">
+        <label className='text-[0.9375rem] font-medium' htmlFor="selectFile">{label}</label>
+        <div className="w-56.5 h-8.75 rounded-xl flex items-center relative bg-transparent">
+            <div className="w-full h-full bg-[#F1F1F1] rounded-xl flex items-center justify-between bg pl-5 pr-3 z-3 pointer-events-none">
+                <h3 className={`text-[0.90rem] truncate ${inputValue? "text-black" : "text-[#787878]"} font-medium`}>{inputValue || "Select Date"}</h3>
+                <img className='w-5 pointer-events-none' src="/src/assets/calender.svg" alt="" srcset="" />
+            </div>
+            <input className='absolute w-full h-6 text-[0.90rem] text font-medium pl-3'  placeholder='JD' type="date" name="selectFile" id="selectFile" onChange={handleDateChange}/>
+        </div>
+    </div>  
+  )
+}
+
+export default DateSelect
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import TextInput from '../components/TextInput.jsx'
+import FileSelect from '../components/FileSelect.jsx'
+import DateSelect from '../components/DateSelect.jsx'
+import DropdownAddEmployee from '../components/DropdownAddEmployee.jsx'
+import PasswordInput from '../components/PasswordInput.jsx'
 
 const Signup = () => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-        <div className="bg-amber-200 w-2/5 h-3/5"></div>
+    <div className='w-screen h-screen flex justify-center items-center bg-[#f9f9f9]'>
+        <div className="bg-white w-2/5 h-4/5 rounded-3xl">
+        <div className='w-full h-20 flex justify-center items-center'>
+            <h1 className="text-4xl">Signup</h1>
+        </div>
+        <div className="w-full h-123 bg-red-200 grid grid-cols-3 items-center justify-items-center">
+            <TextInput label={"First Name:"} placeholder={"First Name"}/>
+            <TextInput label={"Last Name:"} placeholder={"Last Name"}/>
+            <TextInput label={"Email:"} placeholder={"Email"}/>
+            <FileSelect label={"Choose Avatar:"} placeholder={"Choose Avatar"}/>
+            <DateSelect label={"Date of Birth:"}/>
+            <TextInput label={"Country:"} placeholder={"Country"}/>
+            <TextInput label={"City:"} placeholder={"City"}/>
+            <DropdownAddEmployee label={"Select Role:"} values={["admin", "employee"]}/>
+            <PasswordInput placeholder={"Password"} label={"Enter Password"}/>
+            <PasswordInput label={"Enter Admin Secret:"} placeholder={"Admin Secret"}/>
+        </div>
+        </div>
     </div>
   )
 }
