fd1e9b3a3f07aa1a88ef78b4171e3dbf211d1652
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-03 19:53:54 +0530

    feat: Created signup page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+        <div className="bg-amber-200 w-2/5 h-3/5"></div>
+    </div>
+  )
+}
+
+export default signup
