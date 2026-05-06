0a41eea52a36dd692caf5d0959664c2bae27945f
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-06 20:53:43 +0530

    feat: Completed setup to collect from data

--- frontend/src/components/AddEmployeeModal.jsx
+++ frontend/src/components/AddEmployeeModal.jsx
@@ -9,6 +9,34 @@ import Button from './Button.jsx';
 const AddEmployeeModal = ({ handleShowModal }) => {
   const [showModal, setShowModal] = useState(true)
 
+  const [formData, setFromData] = useState({
+          empId: "",
+          contactNo: "",
+          city: "",
+          salary: "",
+          firstName: "",
+          postalCode: "",
+          education: "",
+          otherField: "",
+          middleName: "",
+          email: "",
+          address: "",
+          yearsOfExp: "",
+          lastName: "",
+          dob: "",
+          role: "",
+          avatar: "",
+          gender: "",
+          country: "",
+          workMode: "",
+          empType: "",
+      })
+
+      const handleFormSubmit = async(e) => {
+          e.preventDefault()
+        }
+        console.log(formData)
+
   return (
     <>
         {showModal && <div className="w-screen h-screen z-10 flex justify-center items-center">
@@ -17,28 +45,48 @@ const AddEmployeeModal = ({ handleShowModal }) => {
                     <h1 className='text-[1.75rem] font-semibold pl-10'>Onboard Employee</h1>
                 </div>
                 <Separator marginY={"my-4"} width={"w-310"}/>
-                <div className='w-full h-128 grid grid-cols-4 grid-rows-5 pl-10 '>
-                    <TextInput label={"Emp ID:"} placeholder={"Emp ID"}/>
-                    <TextInput label={"Contact No:"} placeholder={"Contact No"}/>
-                    <TextInput label={"City:"} placeholder={"City"}/>
-                    <TextInput label={"Salary:"} placeholder={"Salary"}/>
-                    <TextInput label={"First Name:"} placeholder={"First Name"}/>
-                    <TextInput label={"Postal Code:"} placeholder={"Postal Code"}/>
-                    <TextInput label={"Education:"} placeholder={"Education"}/>
-                    <TextInput label={"Other Field:"} placeholder={"Other Field"}/>
-                    <TextInput label={"Middle Name:"} placeholder={"Middle Name"}/>
-                    <TextInput label={"Email:"} placeholder={"Email"}/>
-                    <TextInput label={"Address:"} placeholder={"Address"}/>
-                    <TextInput label={"Years of Experience:"} placeholder={"Years of Experience"}/>
-                    <TextInput label={"Last Name:"} placeholder={"Last Name"}/>
-                    <TextInput label={"Date of Birth:"} placeholder={"Date of Birth"}/>
-                    <TextInput label={"Role:"} placeholder={"Role"}/>
-                    <FileSelect label={"Avatar:"} placeholder={"Choose Avatar"}/>
-                    <DropdownAddEmployee label={"Gender:"} title={"Gender"} values={["Male", "Female"]}/>
-                    <DropdownAddEmployee label={"Country:"} title={"Country"} values={countries}/>
-                    <DropdownAddEmployee label={"Workmode:"} title={"Workmode"} values={["On-Site", "Remote", "Hybrid"]}/>
-                    <DropdownAddEmployee label={"Employement Type:"} title={"Employement Type"} values={["Full Time", "Contract", "Freelance", "Intern"]}/>
-                </div>
+                <form onSubmit={handleFormSubmit} className='w-full h-128 grid grid-cols-4 grid-rows-5 pl-10 '>
+                    <TextInput label={"Emp ID:"} placeholder={"Emp ID"}
+                        onChange={(e) => (setFromData({ ...formData, empId: e.target.value }))}/>
+                    <TextInput label={"Contact No:"} placeholder={"Contact No"}
+                        onChange={(e) => (setFromData({ ...formData, contactNo: e.target.value }))}/>
+                    <TextInput label={"City:"} placeholder={"City"}
+                        onChange={(e) => (setFromData({ ...formData, city: e.target.value }))}/>
+                    <TextInput label={"Salary:"} placeholder={"Salary"}
+                        onChange={(e) => (setFromData({ ...formData, salary: e.target.value }))}/>
+                    <TextInput label={"First Name:"} placeholder={"First Name"}
+                        onChange={(e) => (setFromData({ ...formData, firstName: e.target.value }))}/>
+                    <TextInput label={"Postal Code:"} placeholder={"Postal Code"}
+                        onChange={(e) => (setFromData({ ...formData, postalCode: e.target.value }))}/>
+                    <TextInput label={"Education:"} placeholder={"Education"}
+                        onChange={(e) => (setFromData({ ...formData, education: e.target.value }))}/>
+                    <TextInput label={"Other Field:"} placeholder={"Other Field"}
+                        onChange={(e) => (setFromData({ ...formData, otherField: e.target.value }))}/>
+                    <TextInput label={"Middle Name:"} placeholder={"Middle Name"}
+                        onChange={(e) => (setFromData({ ...formData, middleName: e.target.value }))}/>
+                    <TextInput label={"Email:"} placeholder={"Email"}
+                        onChange={(e) => (setFromData({ ...formData, email: e.target.value }))}/>
+                    <TextInput label={"Address:"} placeholder={"Address"}
+                        onChange={(e) => (setFromData({ ...formData, address: e.target.value }))}/>
+                    <TextInput label={"Years of Experience:"} placeholder={"Years of Experience"}
+                        onChange={(e) => (setFromData({ ...formData, yearsOfExp: e.target.value }))}/>
+                    <TextInput label={"Last Name:"} placeholder={"Last Name"}
+                        onChange={(e) => (setFromData({ ...formData, lastName: e.target.value }))}/>
+                    <TextInput label={"Date of Birth:"} placeholder={"Date of Birth"}
+                        onChange={(e) => (setFromData({ ...formData, dob: e.target.value }))}/>
+                    <TextInput label={"Role:"} placeholder={"Role"}
+                        onChange={(e) => (setFromData({ ...formData, role: e.target.value }))}/>
+                    <FileSelect label={"Avatar:"} placeholder={"Choose Avatar"}
+                        onChange={(e) => (setFromData({ ...formData, avatar: e.target.value }))}/>
+                    <DropdownAddEmployee label={"Gender:"} title={"Gender"} values={["Male", "Female"]}
+                        onChange={(e) => (setFromData({ ...formData, gender: e.target.value }))}/>
+                    <DropdownAddEmployee label={"Country:"} title={"Country"} values={countries}
+                        onChange={(e) => (setFromData({ ...formData, country: e.target.value }))}/>
+                    <DropdownAddEmployee label={"Workmode:"} title={"Workmode"} values={["On-Site", "Remote", "Hybrid"]}
+                        onChange={(e) => (setFromData({ ...formData, workMode: e.target.value }))}/>
+                    <DropdownAddEmployee label={"Employement Type:"} title={"Employement Type"} values={["Full Time", "Contract", "Freelance", "Intern"]}
+                        onChange={(e) => (setFromData({ ...formData, empType: e.target.value }))}/>
+                </form>
                 <div className="w-full h-21 flex">
                     <div className="w-4/6 h-full pt-6 flex flex-col">
                         <Separator width={"w-199"}/>
