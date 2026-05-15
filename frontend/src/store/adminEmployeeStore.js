223a4c956eaddb4f3d3f73c3a93f1045ae3b98c6
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-15 11:09:56 +0530

    feat: Added function in store

--- frontend/src/store/adminEmployeeStore.js
+++ frontend/src/store/adminEmployeeStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { onboardEmployee, getEmployees, searchEmployee, getEmployeeBySalary } from "../services/adminEmployee.js";
+import { onboardEmployee, getEmployees, searchEmployee, getEmployeeBySalary, getEmployeeByFilter } from "../services/adminEmployee.js";
 
 const useAdminEmployeeStore = create((set) => ({
     employees: null,
@@ -52,6 +52,17 @@ const useAdminEmployeeStore = create((set) => ({
     getEmployeeBySalary: async (minSalary, maxSalary) => {
         try {
             const response = await getEmployeeBySalary(minSalary, maxSalary)
+            set({ employees: response.data.data.employees, loading: false })
+            return response.data
+        } catch (err) {
+            set({error: err, loading: false})
+            return err
+        }
+    },
+
+    getEmployeeByFilter: async (gender) => {
+        try {
+            const response = await getEmployeeByFilter(gender)
             set({ employees: response.data.data.employees, loading: false })
             return response.data
         } catch (err) {
