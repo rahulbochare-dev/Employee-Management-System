ce84cbb4b7ed715e96c49859ef7c6499a70671a1
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-21 19:49:35 +0530

    feat: added script to generate leave data

---
+++ backend/src/scripts/generateLeaves.js
@@ -0,0 +1,119 @@
+import mongoose from "mongoose";
+import { Leave } from "../models/leave.model.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+})
+
+const employeeIds = [
+  "6a0c0f30fbd3c6dee1126bf0",
+  "6a0c0f31fbd3c6dee1126bf3",
+  "6a0c0f31fbd3c6dee1126bfc",
+  "6a0c0f32fbd3c6dee1126bff",
+  "6a0c0f32fbd3c6dee1126c02",
+  "6a0c0f32fbd3c6dee1126c05",
+  "6a0c0f32fbd3c6dee1126c08",
+  "6a0c0f33fbd3c6dee1126c0b",
+  "6a0c0f33fbd3c6dee1126c0e",
+  "6a0c0f33fbd3c6dee1126c11",
+  "6a0c0f34fbd3c6dee1126c1d",
+  "6a0c0f34fbd3c6dee1126c20"
+];
+
+const leaveTypes = [
+  "Casual",
+  "Sick",
+];
+
+const statuses = [
+  "Pending",
+  "Approved",
+  "Rejected"
+];
+
+const descriptions = [
+  "Family function leave request.",
+  "Medical emergency and recovery time.",
+  "Vacation planned with family.",
+  "Personal work and documentation.",
+  "Need rest due to health issues.",
+  "Travel leave request.",
+  "Attending marriage ceremony.",
+  "Mental wellness break.",
+  "Work-life balance leave.",
+  "Urgent hometown visit."
+];
+
+function randomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function randomDate(start, end) {
+  return new Date(
+    start.getTime() +
+    Math.random() * (end.getTime() - start.getTime())
+  );
+}
+
+async function generateLeaves() {
+  try {
+    await mongoose.connect("");
+    
+    const leaves = [];
+    
+    for (let i = 0; i < 120; i++) {
+        
+        const fromDate = randomDate(
+            new Date(2025, 0, 1),
+            new Date(2026, 11, 1)
+        );
+        
+        const leaveDays = Math.floor(Math.random() * 15) + 1;
+        
+        const toDate = new Date(fromDate);
+
+        toDate.setDate(fromDate.getDate() + leaveDays);
+        
+      const leave = {
+        employee: new mongoose.Types.ObjectId(
+          randomItem(employeeIds)
+        ),
+        
+        leaveType: randomItem(leaveTypes),
+        
+        description: randomItem(descriptions),
+        
+        from: fromDate,
+        
+        to: toDate,
+        
+        status: randomItem(statuses),
+        
+        createdAt: new Date(),
+
+        updatedAt: new Date()
+    };
+
+      leaves.push(leave);
+    }
+
+    const insertedLeaves = await Leave.insertMany(leaves)
+    console.log(insertedLeaves)
+    console.log(mongoose.connection.name)
+    
+    console.log("✅ Dummy leaves generated successfully");
+    
+    process.exit(0);
+
+  } catch (err) {
+
+    console.log("❌ Error generating leaves");
+
+    console.log(err);
+
+    process.exit(1);
+  }
+}
+
+generateLeaves();
