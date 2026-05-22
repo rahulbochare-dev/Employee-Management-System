41758564966da69b71ecfe5d00a3d1842191773e
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-22 13:28:22 +0530

    feat: added leavedetails component

---
+++ frontend/src/components/LeaveDetails.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import Separator from './Seperator.jsx'
+
+const LeaveDetails = () => {
+  return (
+    <div className="w-304.5 h-155.5 bg-white rounded-[1.875rem] px-8.5 pt-6 overflow-hidden">
+    <h1 className="text-[1.75rem]  font-medium text-black leading-none">
+      Leave Application
+    </h1>
+    <Separator marginY={"my-5"} width={"w-310"} />
+    <div className="w-full h-127.5 flex">     
+      <div className="w-107.5 h-full pr-8 border-r border-[#dddddd]">       
+        <h2 className="text-[1.5rem] font-medium text-black">
+          Employee
+        </h2>
+        <div className="w-full h-28 flex items-center gap-5">       
+          <img className="size-17 rounded-full object-cover shrink-0" src="/src/assets/businessman.png" alt=""/>
+          <div className="flex flex-col">           
+            <h2 className="text-[1.375rem] font-medium text-black leading-none">
+              Lucas Garcia
+            </h2>
+            <h3 className="text-[1rem] text-[#7a7a7a] font-medium mt-2 leading-none">
+              Software Engineer
+            </h3>
+            <h3 className="text-[1rem] text-[#7a7a7a] font-medium mt-2 leading-none">
+              ID: EMP-1011 • lucas11@gmail.com
+            </h3>
+          </div>
+        </div>
+        <Separator marginY={"my-5"} width={"w-full"} />
+        <div className="w-full grid grid-cols-2 gap-y-10 gap-x-20 mt-3 pl-10">        
+          <div className="flex flex-col">
+            <h3 className="text-[1.25rem] text-[#666666] font-medium">
+              Leave Type:
+            </h3>
+            <div className="w-fit px-4 h-8 rounded-xl bg-[#cfc8ff] flex justify-center items-center mt-2">
+              <h2 className="text-[0.875rem] text-[#5446d8] font-medium">
+                Casual
+              </h2>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[1.25rem] text-[#666666] font-medium">
+              Status:
+            </h3>
+            <div className="w-fit px-4 h-8 rounded-xl bg-[#efe6a8] flex justify-center items-center mt-2">
+              <h2 className="text-[0.875rem] text-[#a68500] font-medium">
+                Pending
+              </h2>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[1.25rem] text-[#666666] font-medium">
+              From:
+            </h3>
+            <h2 className="text-[1.25rem] text-black font-medium mt-1.5">
+              Oct 03, 2026
+            </h2>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[1.25rem] text-[#666666] font-medium">
+              To:
+            </h3>
+            <h2 className="text-[1.25rem] text-black font-medium mt-1.5">
+              Oct 07, 2026
+            </h2>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[1.25rem] text-[#666666] font-medium">
+              Duration:
+            </h3>
+            <h2 className="text-[1.25rem] text-black font-medium mt-1.5">
+              5 Days
+            </h2>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[1.25rem] text-[#666666] font-medium">
+              Applied On:
+            </h3>
+            <h2 className="text-[1.25rem] text-black font-medium mt-1.5">
+              May 21, 2026
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 pl-9 flex flex-col">    
+        <h2 className="text-[1.5rem] font-medium text-black">
+          Description:
+        </h2>
+        <div className="w-full h-91 bg-[#f1f1f1] rounded-[1.25rem] mt-3 px-8 py-6 overflow-y-auto">
+          <p className="text-[1.05rem] leading-8 text-black font-normal">
+            I am requesting leave for a few days due to an important
+            family-related matter that requires my presence and attention.
+            Over the past few days, there have been some urgent personal
+            responsibilities at home that I need to manage, and it will not
+            be possible for me to balance both work and these commitments
+            effectively during this period. I have ensured that my current
+            tasks are either completed or properly documented so the workflow
+            is not affected significantly in my absence. I kindly request you
+            to approve my leave for the mentioned duration. I will remain
+            available for any urgent communication if required and will resume
+            work immediately after the leave period ends.
+          </p>
+        </div>
+        <div className="w-full flex justify-end items-center gap-6 mt-8">
+          <button className="w-33.25 h-10.25 rounded-[0.875rem] bg-[#f58484] text-[1rem] font-medium text-black transition-all hover:bg-[#ef7575] active:bg-[#e76767]">
+            Reject
+          </button>
+          <button className="w-33.25 h-10.25 rounded-[0.875rem] bg-[#7ee2a0] text-[1rem] font-medium text-black transition-all hover:bg-[#71d493] active:bg-[#64c786]">
+            Approve
+          </button>
+          <button className="w-33.25 h-10.25 rounded-[0.875rem] border border-[#d3d3d3] bg-[#fafafa] text-[1rem] font-medium text-black transition-all hover:bg-[#f2f2f2] active:bg-[#ebebeb]">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default LeaveDetails
