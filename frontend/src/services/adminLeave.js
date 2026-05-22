b591a9b89077dcceaa2a6d7785f62523b9b65d6a
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-22 10:22:15 +0530

    fix: fixed request type

--- frontend/src/services/adminLeave.js
+++ frontend/src/services/adminLeave.js
@@ -1,7 +1,7 @@
 import { callApi } from "../utils/callAPI.js";
 
 const getLeaves = function async(status){
-    const response = callApi("POST", `/admin/leave/leaves?status=${status}`)
+    const response = callApi("GET", `/admin/leave/leaves?status=${status}`)
     return response
 }
 
