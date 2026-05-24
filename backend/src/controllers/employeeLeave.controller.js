634c94ca27fecb2e4041fb0bf966799a29f0d672
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-24 19:49:18 +0530

    feat: added employee name get leave controller for employee

--- backend/src/controllers/employeeLeave.controller.js
+++ backend/src/controllers/employeeLeave.controller.js
@@ -28,7 +28,7 @@ const addLeave = asyncHandler( async (req, res) => {
 })
 
 const getLeaves = asyncHandler( async (req, res) => {
-    const leaveInDB = await Leave.find({employee: req.employee._id})
+    const leaveInDB = await Leave.find({employee: req.employee._id}).populate("employee", "firstName lastName")
 
     res.status(200).json(new ApiResponse(200, {leaves: leaveInDB}, "Leaves fetched successfully"))
 })
