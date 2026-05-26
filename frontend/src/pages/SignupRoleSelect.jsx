409bc816b72cef7385d876a40c86f0ab80e20615
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-05-26 12:46:39 +0530

    feat: added responsiveness in signup roll select

--- frontend/src/pages/SignupRoleSelect.jsx
+++ frontend/src/pages/SignupRoleSelect.jsx
@@ -6,10 +6,10 @@ const SignupRoleSelect = () => {
     const navigate = useNavigate()
 
   return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center bg-[#f9f9f9]'>
-        <div className="bg-white w-98 h-68 rounded-3xl flex flex-col items-center">
-        <div className='w-full h-20 flex justify-center items-center mb-3'>
-            <h1 className="text-4xl">Signup as</h1>
+    <div className='w-screen min-h-screen flex flex-col justify-center items-center bg-[#f9f9f9] px-4 py-6 overflow-x-hidden'>
+        <div className="bg-white w-full sm:w-[85%] md:w-[60%] lg:w-[40%] xl:w-98 rounded-3xl flex flex-col items-center py-6">
+        <div className='w-full h-20 flex justify-center items-center mb-3 px-4'>
+            <h1 className="text-4xl text-center">Continue as</h1>
         </div>
             <SignupRoleButton onclick={() => {navigate("/signup")}} name={"Admin"}/>
             <SignupRoleButton onclick={() => {navigate("/login-employee")}} name={"Employee"}/>
