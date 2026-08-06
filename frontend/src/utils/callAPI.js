a6e36a7cddb6ccc0eb88f1997d11b68911d7b58a
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-08-06 20:14:51 +0530

    feat: added base url from env for frontend

--- frontend/src/utils/callAPI.js
+++ frontend/src/utils/callAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5050/api/v1";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const callApi = async function (method, url, data) {
   try {
