743ac421147cc115d156d55f4afff52296ea7dce
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-08-06 21:18:38 +0530

    fix: added function to create temp folder

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import { connectDB } from "./src/database/index.js";
+import fs from "fs";
 
 dotenv.config({
     path: "./.env"
 })
+
+if (!fs.existsSync("./public/temp")) {
+  fs.mkdirSync("./public/temp", { recursive: true });
+}
 
 connectDB()
 .then(() => {
