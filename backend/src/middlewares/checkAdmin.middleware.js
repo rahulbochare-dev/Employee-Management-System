e368817cde1672ca7cb749018969320faf2ef6c9
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-08-06 13:12:53 +0530

    fix: changed import statement

--- backend/src/middlewares/checkAdmin.middleware.js
+++ backend/src/middlewares/checkAdmin.middleware.js
@@ -1,6 +1,6 @@
 import  jwt  from "jsonwebtoken"
 import { ApiError } from "../utils/ApiError.js"
-import { User } from "../Models/user.model.js"
+import { User } from "../models/user.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const checkIsAdmin = asyncHandler( async (req, res, next) => {
